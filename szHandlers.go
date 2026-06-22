dbf78c728a58c2a8d05f5a7d6a8ac5ba288bfcf1
esmangalova-max <e.s.mangalova@gmail.com>
2026-06-22 17:11:04 +0700

    1 commit

---
+++ szHandlers.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/LindsayBradford/go-dbf/godbf"
+	"github.com/go-faster/errors"
+	"github.com/google/uuid"
+
+	"ParseReestrLZP/cnfg"
+	"ParseReestrLZP/dbase"
+	"ParseReestrLZP/sqlScripts"
+	"ParseReestrLZP/unarch"
+
+	_ "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/schollz/progressbar/v3"
+)
+
+func HandleSz(arcname string, zf *dbase.ZFiles) error {
+	data, err := unarch.Decopress(arcname, zf)
+	if err != nil {
+		return errors.Errorf("unarch.UnarcSZ(%q): %w", arcname, err)
+	}
+	szlData := (*data)["sz_l.dbf"]
+	err = SaveSzL(&szlData, zf)
+	if err != nil {
+		return errors.Errorf("SaveSzL(%q): %w", arcname, err)
+	}
+	szpData := (*data)["sz_p.dbf"]
+	err = SaveSzP(&szpData, zf)
+	if err != nil {
+		return errors.Errorf("SaveSzP(%q): %w", arcname, err)
+	}
+	err = MoveSZToArchWOTran()
+	if err != nil {
+		return errors.Errorf("MoveSZToArch(%q): %w", arcname, err)
+	}
+	err = TmpToCurrent()
+	if err != nil {
+		return errors.Errorf("TmpToCurrent(): %w", err)
+	}
+
+	err = RefreshPersonList()
+	if err != nil {
+		return errors.Errorf("RefreshPersonList(): %w", err)
+	}
+
+	err = AddFilesRecord(zf, 0)
+	if err != nil {
+		return errors.Errorf("AddFilesRecord(%q): %w", arcname, err)
+	}
+	return nil
+}
+
+
